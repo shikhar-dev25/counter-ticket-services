1a7feca510796f1569f6bd439877fd19dd93fea7
Shikhar <shikhar.maurya2504@gmail.com>
2025-12-07 00:40:12 +0530

    otp-verification page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { TicketForm } from './pages/ticket-form/ticket-form';
+import { OtpVerification } from './pages/otp-verification/otp-verification';
+import { PassengerDetails } from './pages/passenger-details/passenger-details';
 
 export const routes: Routes = [
   {
@@ -8,7 +10,9 @@ export const routes: Routes = [
     component: MainLayout,
     children: [
       { path: '', redirectTo: 'ticket-form', pathMatch: 'full' },
-      { path: 'ticket-form', component: TicketForm }
+      { path: 'ticket-form', component: TicketForm },
+      { path: 'otp', component: OtpVerification},
+      { path: 'passenger-details', component: PassengerDetails}
     ]
   }
 ];
---
+++ src/app/pages/passenger-details/passenger-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passenger-details',
+  imports: [],
+  templateUrl: './passenger-details.html',
+  styleUrl: './passenger-details.css',
+})
+export class PassengerDetails {
+
+}
